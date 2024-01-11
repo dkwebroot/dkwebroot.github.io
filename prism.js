5da937111f4aae5328f398534f6d585f54d9eb4d
Dkwebpoint <126847412+Dkwebpoint@users.noreply.github.com>
2024-01-11 20:09:33 +0100

    Update prism.js

--- prism.js
+++ prism.js
@@ -1,3 +1,33 @@
+function get_action(form)
+{
+    var v = grecaptcha.getResponse();
+
+    if(v.length == 0 || v == undefined || v == "")
+    {
+        document.getElementById('captcha').innerHTML="Captcha obligatoire!<br>Cochez la case <span style='color:red;'>je ne suis pas un robot</span>";
+        return false;
+    }
+    else
+    {
+        document.getElementById('captcha').innerHTML="Captcha completed";
+        return true;
+    }
+}
+// function non utilisee
+/*
+
+ $('#toaffich').on('click', function() {
+        if(document.getElementById('a_manipuler').style.display == "none") {
+            $('#a_manipuler').fadeIn();
+            $('#toaffich').html('Lire -');
+        } else {
+            $('#a_manipuler').fadeOut();
+            setTimeout(function() {
+                $('#toaffich').html('Lire +');
+            }, 450);
+        }
+    });
+*/
 /* PrismJS 1.29.0
 https://prismjs.com/download.html#themes=prism&languages=markup+css+clike+javascript+javadoclike+markup-templating+nasm+nim+perl+php+phpdoc+php-extras+python&plugins=line-numbers+toolbar+copy-to-clipboard */
 /// <reference lib="WebWorker"/>
